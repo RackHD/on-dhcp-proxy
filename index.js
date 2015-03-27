2dd002ff5e6bdd70bdf90afac22dd86bf2e8be61
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-03-26 17:21:39 -0700

    Updates to get things running

--- index.js
+++ index.js
@@ -8,9 +8,24 @@ var di = require('di'),
     core = require('on-core')(di),
     injector = new di.Injector(
         _.flatten([
-            core.injectables
+            core.injectables,
+            core.helper.requireGlob(__dirname + '/lib/**/*.js')
         ])
     ),
-    Logger = injector.get('Logger');
+    Logger = injector.get('Logger'),
+    logger = Logger.initialize('Dhcp'),
+    Server = injector.get('DHCP.Proxy.Server');
 
-var logger = Logger.initialize('Dhcp');
+var server = Server.create(4011, 68, '10.1.1.1', '10.1.1.255');
+
+try {
+    server.start();
+    logger.info("Starting Proxy DHCP server on 10.1.1.1:4011");
+} catch (e) {
+    logger.error("Error starting server", {
+        error: e
+    });
+    process.nextTick(function() {
+        process.exit(1);
+    });
+}
