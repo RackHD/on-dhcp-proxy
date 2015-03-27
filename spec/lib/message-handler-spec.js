345a8463d9b121b2c66b8b863d0ad3f43ffb4cfe
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-03-26 17:22:01 -0700

    Add a slew of tests for MessageHandler related functionality

--- spec/lib/message-handler-spec.js
+++ spec/lib/message-handler-spec.js
@@ -28,8 +28,128 @@ describe("MessageHandler", function() {
 
     beforeEach("MessageHandler beforeEach", function() {
         packetData = {
-            chaddr: '00:00:00:00:00:00'
+            chaddr: {
+                address: '00:00:00:00:00:00'
+            },
+            options: {}
         };
+    });
+
+    describe("ActionHandler", function() {
+        var actionHandler;
+        var Errors;
+        var thrown;
+        var nextFn = function () {};
+
+        beforeEach("ActionHandler beforeEach", function() {
+            Errors = helper.injector.get('Errors');
+            actionHandler = messageHandler.createActionHandler(packetData);
+            sinon.stub(actionHandler, 'getDefaultBootfile');
+            thrown = false;
+        });
+
+        it("should resolve default bootfile on discover action", function() {
+            var testDefaultBootfile = 'test-default-bootfile';
+            actionHandler.getDefaultBootfile.returns(testDefaultBootfile);
+
+            return Q.resolve()
+            .then(function() {
+                return actionHandler.handleAction(nextFn, 'discover', undefined);
+            })
+            .catch(function(error) {
+                thrown = true;
+                expect(error).to.be.an.instanceof(Errors.BreakPromiseChainError);
+            })
+            .then(function() {
+                expect(thrown).to.equal(true);
+                return actionHandler.deferred.promise;
+            })
+            .then(function(out) {
+                expect(out).to.equal(testDefaultBootfile);
+                expect(actionHandler.getDefaultBootfile)
+                    .to.have.been.calledWith(actionHandler.packetData);
+            });
+        });
+
+        it("should do nothing on ignore action", function() {
+            return Q.resolve()
+            .then(function() {
+                return actionHandler.handleAction(nextFn, 'ignore', undefined);
+            })
+            .catch(function(error) {
+                thrown = true;
+                expect(error).to.be.an.instanceof(Errors.BreakPromiseChainError);
+            })
+            .then(function() {
+                expect(thrown).to.equal(true);
+                return actionHandler.deferred.promise;
+            })
+            .then(function(out) {
+                expect(out).to.equal(null);
+            });
+        });
+
+        it("should call next on next action", function() {
+            var nextOut = { action: 'test' };
+            var nextStub = sinon.stub().resolves(nextOut);
+            var data = 'testdata';
+
+            return Q.resolve()
+            .then(function() {
+                return actionHandler.handleAction(nextStub, 'next', data);
+            })
+            .then(function(out) {
+                expect(out).to.equal(nextOut);
+                expect(nextStub).to.have.been.calledWith(data);
+            });
+        });
+
+        it("should resolve default bootfile on send-default-bootfile action", function() {
+            var testDefaultBootfile = 'test-default-bootfile';
+            actionHandler.getDefaultBootfile.returns(testDefaultBootfile);
+
+            return Q.resolve()
+            .then(function() {
+                return actionHandler.handleAction(nextFn, 'send-default-bootfile', undefined);
+            })
+            .catch(function(error) {
+                thrown = true;
+                expect(error).to.be.an.instanceof(Errors.BreakPromiseChainError);
+            })
+            .then(function() {
+                expect(thrown).to.equal(true);
+                return actionHandler.deferred.promise;
+            })
+            .then(function(out) {
+                expect(out).to.equal(testDefaultBootfile);
+                expect(actionHandler.getDefaultBootfile)
+                    .to.have.been.calledWith(actionHandler.packetData);
+            });
+        });
+
+        it("should resolve task-specified bootfile on send-custom-bootfile action", function() {
+            var testCustomBootfile = 'test-custom-bootfile';
+
+            return Q.resolve()
+            .then(function() {
+                return actionHandler.handleAction(
+                    nextFn, 'send-custom-bootfile', testCustomBootfile);
+            })
+            .catch(function(error) {
+                thrown = true;
+                expect(error).to.be.an.instanceof(Errors.BreakPromiseChainError);
+            })
+            .then(function() {
+                expect(thrown).to.equal(true);
+                return actionHandler.deferred.promise;
+            })
+            .then(function(out) {
+                expect(out).to.equal(testCustomBootfile);
+            });
+        });
+
+        it("should throw on an unrecognized action", function() {
+        });
     });
 
     describe("getNodeAction", function() {
@@ -52,16 +172,17 @@ describe("MessageHandler", function() {
 
         it("should call lookupService.macAddressToNodeId with node mac address", function() {
             lookupService.macAddressToNodeId.resolves('');
-            return messageHandler.getNodeAction(packetData.chaddr)
+            return messageHandler.getNodeAction(packetData.chaddr.address)
             .then(function() {
-                expect(lookupService.macAddressToNodeId).to.have.been.calledWith(packetData.chaddr);
+                expect(lookupService.macAddressToNodeId)
+                    .to.have.been.calledWith(packetData.chaddr.address);
             });
         });
 
         it("should get the node action for a new node", function() {
             lookupService.macAddressToNodeId.rejects(new Errors.LookupError(''));
 
-            return messageHandler.getNodeAction(packetData.chaddr)
+            return messageHandler.getNodeAction(packetData.chaddr.address)
             .then(function(out) {
                 expect(out).to.have.property('action').that.equals('discover');
             });
@@ -70,7 +191,7 @@ describe("MessageHandler", function() {
         it("should ignore a node on an unknown lookup failure", function() {
             lookupService.macAddressToNodeId.rejects(new Error(''));
 
-            return messageHandler.getNodeAction(packetData.chaddr)
+            return messageHandler.getNodeAction(packetData.chaddr.address)
             .then(function(out) {
                 expect(out).to.have.property('action').that.equals('ignore');
             });
@@ -79,7 +200,7 @@ describe("MessageHandler", function() {
         it("should get the action for a node if it is determined to be already known", function() {
             lookupService.macAddressToNodeId.resolves(testnodeid);
 
-            return messageHandler.getNodeAction(packetData.chaddr)
+            return messageHandler.getNodeAction(packetData.chaddr.address)
             .then(function(out) {
                 expect(out).to.have.property('action').that.equals('next');
                 expect(out).to.have.property('data').that.equals(testnodeid);
@@ -198,7 +319,7 @@ describe("MessageHandler", function() {
 
         it("should return the profiles API for an iPXE user class", function() {
             var expectedUrl = 'http://10.1.1.1:80/api/common/profiles';
-            packetData.userClass = 'iPXE';
+            packetData.options.userClass = 'iPXE';
 
             configuration.get.withArgs('server').returns('10.1.1.1');
             configuration.get.withArgs('httpPort').returns('80');
@@ -206,8 +327,9 @@ describe("MessageHandler", function() {
         });
 
         it("should return the profiles API with mac params for arista nodes", function() {
-            var expectedUrl = 'http://10.1.1.1:80/api/common/profiles?macs=' + packetData.chaddr;
-            packetData.vendorClassIdentifier = 'Arista';
+            var expectedUrl =
+                'http://10.1.1.1:80/api/common/profiles?macs=' + packetData.chaddr.address;
+            packetData.options.vendorClassIdentifier = 'Arista';
 
             configuration.get.withArgs('server').returns('10.1.1.1');
             configuration.get.withArgs('httpPort').returns('80');
@@ -216,7 +338,7 @@ describe("MessageHandler", function() {
 
         it("should return the profiles API for mellanox mac addresses", function() {
             var expectedUrl = 'http://10.1.1.1:80/api/common/profiles';
-            packetData.chaddr = '00:02:c9:00:00:00';
+            packetData.chaddr.address = '00:02:c9:00:00:00';
 
             configuration.get.withArgs('server').returns('10.1.1.1');
             configuration.get.withArgs('httpPort').returns('80');
@@ -224,13 +346,103 @@ describe("MessageHandler", function() {
         });
 
         it("should return intel.ipxe for intel mac addresses", function() {
-            packetData.chaddr = 'ec:a8:6b:00:00:00';
+            packetData.chaddr.address = 'ec:a8:6b:00:00:00';
             expect(messageHandler.getDefaultBootfile(packetData)).to.equal('intel.ipxe');
         });
 
         it("should return the default ipxe script if no special cases are met", function() {
-            packetData.vendorClassIdentifier = 'testVendorClass';
+            packetData.options.vendorClassIdentifier = 'testVendorClass';
             expect(messageHandler.getDefaultBootfile(packetData)).to.equal('renasar.ipxe');
+        });
+    });
+
+    describe("getBootfile", function() {
+        before("MessageHandler.getBootfile before", function() {
+            sinon.stub(messageHandler, 'getDefaultBootfile');
+            sinon.stub(messageHandler, 'getNodeAction');
+            sinon.stub(messageHandler, 'getKnownNodeAction');
+            sinon.stub(messageHandler, 'getKnownNodeActionFromTask');
+        });
+
+        beforeEach("MessageHandler.getBootfile beforeEach", function() {
+            messageHandler.getDefaultBootfile.reset();
+            messageHandler.getNodeAction.reset();
+            messageHandler.getKnownNodeAction.reset();
+            messageHandler.getKnownNodeActionFromTask.reset();
+        });
+
+        after("MessageHandler.getBootfile after", function() {
+            messageHandler.getDefaultBootfile.restore();
+            messageHandler.getNodeAction.restore();
+            messageHandler.getKnownNodeAction.restore();
+            messageHandler.getKnownNodeActionFromTask.restore();
+        });
+
+        it("should return a bootfile for an unknown node", function() {
+            var bootfile = 'test.ipxe';
+            messageHandler.getNodeAction.resolves({ action: 'discover' });
+            messageHandler.getDefaultBootfile.returns(bootfile);
+
+            return messageHandler.getBootfile(packetData)
+            .then(function(_bootfile) {
+                expect(messageHandler.getNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeAction).to.not.have.been.called;
+                expect(messageHandler.getKnownNodeActionFromTask).to.not.have.been.called;
+                expect(_bootfile).to.equal(bootfile);
+            });
+        });
+
+        it("should not return a bootfile for a known node with no active task", function() {
+            var testnodeid = 'testnodeid';
+            messageHandler.getNodeAction.resolves({ action: 'next', data: testnodeid });
+            messageHandler.getKnownNodeAction.resolves({ action: 'ignore' });
+
+            return messageHandler.getBootfile(packetData)
+            .then(function(_bootfile) {
+                expect(messageHandler.getNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeAction).to.have.been.calledWith(testnodeid);
+                expect(messageHandler.getKnownNodeActionFromTask).to.not.have.been.called;
+                expect(_bootfile).to.equal(null);
+            });
+        });
+
+        it("should return a default bootfile for known node with an active task", function() {
+            var bootfile = 'test.ipxe';
+            var testnodeid = 'testnodeid';
+            messageHandler.getNodeAction.resolves({ action: 'next', data: testnodeid });
+            messageHandler.getKnownNodeAction.resolves({ action: 'next', data: testnodeid });
+            messageHandler.getKnownNodeActionFromTask.resolves({ action: 'send-default-bootfile' });
+            messageHandler.getDefaultBootfile.returns(bootfile);
+
+            return messageHandler.getBootfile(packetData)
+            .then(function(_bootfile) {
+                expect(messageHandler.getNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeActionFromTask).to.have.been.calledOnce;
+                expect(_bootfile).to.equal(bootfile);
+            });
+        });
+
+        it("should return a custom bootfile for known node with an active task", function() {
+            var bootfile = 'testcustom.ipxe';
+            messageHandler.getNodeAction.resolves({ action: 'next', data: testnodeid });
+            messageHandler.getKnownNodeAction.resolves({ action: 'next', data: testnodeid });
+            messageHandler.getKnownNodeActionFromTask.resolves(
+                { action: 'send-custom-bootfile', data: bootfile }
+            );
+
+            return messageHandler.getBootfile(packetData)
+            .then(function(_bootfile) {
+                expect(messageHandler.getNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeAction).to.have.been.calledOnce;
+                expect(messageHandler.getKnownNodeActionFromTask).to.have.been.calledOnce;
+                expect(_bootfile).to.equal(bootfile);
+            });
+        });
+
+        it("should reject on internal failures getting node data", function() {
+            messageHandler.getNodeAction.rejects(new Error('Test Error'));
+            return expect(messageHandler.getBootfile(packetData)).to.be.rejectedWith(/Test Error/);
         });
     });
 });
