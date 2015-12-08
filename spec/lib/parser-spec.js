78a77868aefc7840b3005dbb3ea963a79a9738d1
Tracy Gius <tracy.gius@emc.com>
2015-12-08 13:50:10 -0800

    adding tests for readIp and readString

--- spec/lib/parser-spec.js
+++ spec/lib/parser-spec.js
@@ -6,6 +6,9 @@
 describe("Parser", function() {
 
     var parser;
+    var msg;
+    var offset;
+
 
     before('Parser before', function() {
         helper.setupInjector(
@@ -27,16 +30,16 @@ describe("Parser", function() {
 
     describe('readIpRaw Function', function() {
         it("should return undefined if the msg offset is 0", function() {
-            var msg = new Buffer(2);
-            var offset = 0;
+            msg = new Buffer(2);
+            offset = 0;
             msg[0] = 0x0;
 
             expect(parser.readIpRaw(msg, offset)).to.be.an('undefined');
         });
 
         it("should return the msg as a string seperated by \'.\'", function() {
-            var msg = new Buffer(4);
-            var offset = 0;
+            msg = new Buffer(4);
+            offset = 0;
             msg[0] = 0x3;
             msg[1] = 0x4;
             msg[2] = 0x5;
@@ -56,7 +59,64 @@ describe("Parser", function() {
         });
     });
 
-    describe('readString Function', function() {
+    describe('ReadIp Function', function() {
+        beforeEach("ReadIp beforeEach", function() {
+            msg = new Buffer(3);
+            offset = 0;
+            msg[0] = 0x4;
+
+            parser.packet = {
+                options: {}
+            };
+        });
+
+        it("should return the offset plus the len and update the packet options names", function() {
+            var len = msg.readUInt8(offset);
+
+            expect(parser.readIp(msg, offset, "test_name")).to.equal(offset+len+1);
+        });
+
+        it("should throw assert false if the length does not equal 4", function() {
+            expect(function() {
+                msg[0] = 0x3;
+
+                parser.readIp(msg, offset, "test_name");
+            }).to.throw(Error, '3 === 4');
+        });
+
+        it("should update the packet options names", function(){
+            var len = msg.readUInt8(offset);
+
+            expect(parser.readIp(msg, offset, "test_name")).to.equal(offset+len+1);
+            expect(parser.packet.options).to.include.keys("test_name");
+        });
+    });
+
+    describe('ReadString Function', function() {
+
+        beforeEach("ReadString beforeEach", function() {
+            msg = new Buffer(3);
+            offset = 0;
+            msg[0] = 0x3;
+
+            parser.packet = {
+                options: {}
+            };
+        });
+
+        it("should return the offset and update the packet options names", function() {
+            var len = msg.readUInt8(offset);
+
+            expect(parser.readString(msg, offset, "test_name")).to.equal(offset+len+1);
+        });
+
+        it("should update the packet options names", function(){
+            var len = msg.readUInt8(offset);
+
+            expect(parser.readString(msg, offset, "test_name")).to.equal(offset+len+1);
+            expect(parser.packet.options).to.include.keys("test_name");
+        });
+
 
     });
 
@@ -72,7 +132,7 @@ describe("Parser", function() {
         });
 
         it("should return the address when the length is greater 0 it will add a \':\' to the address", function() {
-            var msg = new Buffer(2);
+            msg = new Buffer(2);
             var offset = 0;
             msg[0] = 0x3;
             msg[1] = 0x4;
